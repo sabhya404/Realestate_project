b542ca31c0efb4fae05f6b02d6dc59faf415859d
sabhya-beginner <agarwalsabhya44@gmail.com>
2024-07-08 18:42:20 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import SignUp from "./pages/Signup";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
